56a123c7600994118bdde800365dc2ba38eac6a4
Jason Melton <jason.melton2@gmail.com>
2020-01-09 06:50:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague,"
+        :name => "Lord Montague",
         :age => "53"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => { 
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
